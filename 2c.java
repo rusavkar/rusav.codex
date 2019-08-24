bd4df5e27509451d55eede9641844c2c7ee7b496
rusavkar <53941439+rusavkar@users.noreply.github.com>
2019-08-24 13:34:52 +0530

    codex

---
+++ 2c.java
@@ -0,0 +1,23 @@
+public class 2c 
+{
+    public static void main(string[] args)
+    {
+        char c='a' ;
+        System.out.print(c);
+        int b=c;
+        System.out.print(b);
+        int d=b+1;
+        int e=d%2;
+        if (e==0)
+        {
+            System.out.print("true");
+
+
+        }else{
+            System.out.print("false");
+        }
+        
+        
+
+    }
+}
