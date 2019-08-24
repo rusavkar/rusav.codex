54188c6d49fa106cbf286af168987e9e146901de
rusavkar <53941439+rusavkar@users.noreply.github.com>
2019-08-24 13:47:42 +0530

    codex

---
+++ 3a.java
@@ -0,0 +1,10 @@
+public class 3a
+{
+    public static void main(Strings[] args)
+    {
+        int a=8;
+        int x;
+        x=a++ + ++a + a + ++a + a++;
+        System.out.println(x + "," + a);
+    }
+}
