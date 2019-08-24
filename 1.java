eb04b24e96f4aa9a8cd04d898b75e0b3205d0876
rusavkar <53941439+rusavkar@users.noreply.github.com>
2019-08-24 13:29:16 +0530

    codex

---
+++ 1.java
@@ -0,0 +1,14 @@
+public class 1
+{
+    public static void main(String[] args)
+    {
+        int a=25,b=30,c=50;
+        greatestnumber=if (a>b && a>c) ? a: if (b>c) ? b:c;
+        System.out.print(greatestnumber);
+
+
+    }
+        
+                
+
+}
---
+++ 2b.java
@@ -0,0 +1,8 @@
+public calss 2b
+{
+    public static void main(string[] args)
+    {
+        float a=5,b=2;
+        System.out.print(a/b);
+    }
+}
