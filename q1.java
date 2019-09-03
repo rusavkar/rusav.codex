bd5ca3f16b3916561ce0e9a23e9fc4fc9c34fabc
rusavkar <53941439+rusavkar@users.noreply.github.com>
2019-09-03 20:26:57 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,17 @@
+
+public class q1 {
+
+	public static void main(String[] args) {
+		int sum = 0 ,i;
+		for( i=0;i<1000;i++);
+		{
+			if(i % 5==0||i % 3==0);
+			sum=+i;
+			System.out.println(sum);
+			
+		}
+		
+
+	}
+
+}
